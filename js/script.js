3a53ef6057eb08e278323596180e005fa3a087c2
Hai Tran <159185497+hai-tran-2@users.noreply.github.com>
2024-05-24 18:50:16 +0000

    add webpage

--- js/script.js
+++ js/script.js
@@ -1,5 +1,25 @@
 // Copyright (c) 2020 Mr. Coxall All rights reserved
 //
-// Created by: Mr. Coxall
-// Created on: Sep 2020
+// Created by: Hai Tran
+// Created on: May 2024
 // This file contains the JS functions for index.html
+
+"use strict"
+
+window.onload = function () {
+  // this calculates volume of a sphere
+  const params = new URLSearchParams(document.location.search)
+
+  // input
+  const radiusOfSphere = params.get("radiusOfSphere")
+  console.log(radiusOfSphere)
+
+  // process
+  const volumeOfSphere = (4 * Math.PI * radiusOfSphere ** 3) / 3
+  console.log(volumeOfSphere)
+
+  // output
+  document.getElementById("dimension").innerHTML = "The radius of sphere is: " + radiusOfSphere
+  document.getElementById("result").innerHTML = "The volume is: " + volumeOfSphere.toFixed(2) + " mm³"
+}
+
